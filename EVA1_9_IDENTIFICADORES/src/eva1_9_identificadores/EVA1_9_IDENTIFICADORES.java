92f736ab773f01155bbcfcba12b2cdf523927b2e
L23550334 <143562935+L23550334@users.noreply.github.com>
2023-09-13 17:53:37 -0600

    Add files via upload

---
+++ EVA1_9_IDENTIFICADORES/src/eva1_9_identificadores/EVA1_9_IDENTIFICADORES.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_9_identificadores;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_9_IDENTIFICADORES {
+
+    /**
+     * @param args the command line arguments
+     */
+    @SuppressWarnings("empty-statement")
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // int 1nombre: no válido, por empezar con un número.
+        int $nombre;
+        int _nombre = 0;
+        int nombre;
+        int NOMBRE;
+        int Nombre;
+        int nombre_dato;
+        //int nombre; la variable ya existe previamente.
+        
+        // int public; no podemos usar public, es una palabra reservada (keyword).
+        int Public;//si se púede por ser P mayúscula.
+        double salario;
+    }
+    
+}       
