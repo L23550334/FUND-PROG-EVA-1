797af4cfaae913811c274858f25619a537fabc0e
L23550334 <143562935+L23550334@users.noreply.github.com>
2023-09-18 17:51:22 -0600

    Add files via upload

---
+++ EVA1_10_INICIALIZACIÓN/src/eva1_10_inicialización/EVA1_10_INICIALIZACIÓN.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_10_inicialización;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_10_INICIALIZACIÓN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int valor = 0;//La variable es: valor de tipo int;INICIALIZANOD LA VARIBALE o que el usuario le asigne un valor
+        System.out.println(valor);
+
+        int precio;//declarar
+        precio = 100;// inicializar
+
+        double salario = 0, precioMayoreo = 100, iva = 10.5, subtotal = 15, total = 100;//Tenemos 5 variables de tipo double
+
+        //CONSTANTES
+        //FINAL= Te permite asignar un valor solo una vez
+        final int conteo;
+        conteo = 100;
+        System.out.println(conteo);
+
+        //Cuando son constates se usan mayusculas y guion bajo
+        final double MI_VALOR_PI = 3.1416;
+        final String ESCUELA = "Insituto TecnoLógico de Chihuahua ll";
+        final int CALIFA_PASAR = 70;
+        /*
+        
+        Varias líneas de comentarios
+        Cuando quieren tirar mucho rollo
+        esta es la mejor opción
+        */
+        //COMENTARIO DE UNA SOLA LÍNEA, TODA EN LA LINEA ES UN COMENTARIO
+
+    }
+}
